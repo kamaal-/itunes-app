588c67732afeb24fd3c928bd2bb38609f62a99a2
Kamaal Aboothalib <kamaal@kamaal.me>
2017-10-07 16:59:54 +0400

    Initial commit

---
+++ src/services/index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by kamaal on 10/7/17.
+ */
+
+/*
+ * All Redux action types in common place to be access from reducer and action
+ */
+export const SEARCH_TEXT_UPDATED = 'SEARCH_TEXT_UPDATED'
+
+/*
+ * Fetch actions
+ */
+export const ITUNES_FETCH_STARTED = 'ITUNES_FETCH_STARTED' // dispatch when search fetch started
+export const ITUNES_FETCH_SUCCESS = 'ITUNES_FETCH_SUCCESS' // dispatch after search fetch returns with data
+export const ITUNES_FETCH_FAILED = 'ITUNES_FETCH_FAILED'  // dispatch if search fetch returns fail status or fetch fails
+
+/*
+ * Favorite albums
+ * Once user clicked favorite button 'FAVORITE_ALBUM_ADD_REMOVE_STARTED' will be dispatch and action will toggle favorite item
+ */
+export const FAVORITE_ALBUM_ADD_REMOVE_STARTED =  'FAVORITE_ALBUM_ADD_REMOVE_STARTED' // dispatch just after favorite button has pressed
+export const FAVORITE_ALBUM_ADDED =  'FAVORITE_ALBUM_ADDED' // will dispatch if favorite album added
+export const FAVORITE_ALBUM_REMOVED =  'FAVORITE_ALBUM_REMOVED' // will dispatch if favorite album removed
+
+/*
+ * Favorite artist filtering
+ * */
+export const FAVORITE_ARTIST_FILTERED =  'FAVORITE_ARTIST_FILTERED' // will dispatch if favorite artist filtered
