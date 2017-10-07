6230284610637d87a80bdaa3af5817fed45c5fa9
Kamaal Aboothalib <kamaal@kamaal.me>
2017-10-07 17:29:18 +0400

    Main sass file added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import './App.css' // should import before all components to avoid style override
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          Hello world
+      <div className="app">
+          <div className="app__wrap">
+            <Header/>
+            Hello world
+          </div>
       </div>
     );
   }
